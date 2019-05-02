203b6b074fb848ed63398cb8e77bf8b91e37b444
Dwayne Williams <dewilliams0327@gmail.com>
2019-05-02 17:27:43 -0500

    editting theme tab

--- phantom/preferences/tabs/theme_tab.py
+++ phantom/preferences/tabs/theme_tab.py
@@ -12,8 +12,13 @@ class theme_tab(QWidget):
     def __init__(self, mw):
         super().__init__()
         self.setLayout(QVBoxLayout())
+        self.layout().setSpacing(0)
+        self.layout().setContentsMargins(0, 0, 0, 0)
+        self.layout().setStretch(0, 0)
+
         self.mw=mw
         mypath = "phantom/application_settings/themes/"
+
         onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
         
         self.selected_theme = None
@@ -22,34 +27,48 @@ class theme_tab(QWidget):
             try:
                 self.layout().addWidget(self.create_theme_obj(mypath+f))
             except Exception as err:
-                continue
-                # print(f + " not a theme.\n" + str(err))
+                # continue
+                print(f + " not a theme.\n" + str(err))
         # self.layout().addWidget(self.create_theme_obj())
         # print(onlyfiles)
 
     def create_theme_obj(self, fp=None):
-        scheme = json.load(open(fp))["color_scheme"]
-        
-        block = QWidget()
-        block.setLayout(QHBoxLayout())
+        theme = json.load(open(fp))
+
+        scene = QGraphicsScene()
+        view = QGraphicsView(scene)
+        view.setStyleSheet("background: transparent")
+
+        block = QGraphicsWidget()
+        block.setLayout(QGraphicsLinearLayout())
         block.layout().setContentsMargins(0, 0, 0, 0)
+        block.layout().setSpacing(0)
 
         # scheme = {"blue":Qt.blue, "red":Qt.red, "green":Qt.green, "black":Qt.black}
-
-        for key in scheme:
-            b = QWidget()
+        
+        for key in theme["color_scheme"]:
+            b = QGraphicsWidget()
             b.setObjectName("scheme")
             b.setFocusPolicy(Qt.ClickFocus)
+            b.setAutoFillBackground(True)
 
             p = QPalette()
-            p.setColor(QPalette.Background, QColor(scheme[key]))
-            b.setAutoFillBackground(True)
-            b.setPalette(p)
-            block.layout().addWidget(b)
+            p.setColor(QPalette.Background, QColor(theme["color_scheme"][key]))
 
+            b.setPalette(p)
             b.mousePressEvent = (lambda e: self.load_theme(fp))
 
-        return block
+            block.layout().addItem(b)
+
+        name = QGraphicsTextItem()
+        name.setDefaultTextColor(QColor(theme["color_scheme"]["text"]))
+        name.setPos(150,15)
+        name.setPlainText(theme["name"])
+
+        scene.addItem(block)
+        scene.addItem(name)
+
+        return view
 
     def load_theme(self, file):
         self.selected_theme = file
