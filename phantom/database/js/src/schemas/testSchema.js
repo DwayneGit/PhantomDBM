f1122e7e5879b830c59cfe214354dc70ff6f4d8e
Dwayne <dewilliams0327@gmail.com>
2019-07-02 05:26:42 -0500

    mv schemas fldr out of repo

--- phantom/database/js/src/schemas/testSchema.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose")
-var testSchema = new mongoose.Schema({
-    "Name": String,
-	"Age": Number,
-	"Relationship_Status": String
-}, {"collection": "test"})
-
-module.exports = mongoose.model("testModel",testSchema)
--- phantom/database/js/src/schemas/test_testToo/testTooSchema.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose")
-var testTooSchema = new mongoose.Schema({
-			Name : String,
-			Age : Number
-		}, {
-			collection: "testToo"
-		})
-
-module.exports = mongoose.model("testTooModel",testTooSchema)
